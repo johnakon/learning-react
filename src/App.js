8918d1fe5b58d49b2798247f608e52fc637d64d9
johnakon <john.akonya@gmail.com>
2020-02-17 16:34:10 +0300

    App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router , Route, Switch, Link} from 'react-router-dom';
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
+
+class App extends Component {
+  render(){
+    return(
+      <Router>
+        <h2 className="heading1">
+          Learning React Router
+        </h2>
+        {/* navigation */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto" id="list">
+            <li><Link to={'/'} className="nav-link">Home</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+          </ul>
+        </nav>
+        <hr />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+      </Router>
+    )
+  }
 }
+
 
 export default App;
